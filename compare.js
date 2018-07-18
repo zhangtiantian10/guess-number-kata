f7babc9509ff8b135c56d17e76084c5b7538e148
Tian Zhang <tizhang@CNtizhang.local>
2018-07-18 15:28:06 +0800

    write compare input and answer, input and answer is stable
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

---
+++ compare.js
@@ -0,0 +1,19 @@
+const compareInputAnswer = (input = "1 2 3 4", answer = "1 2 3 4") => {
+    const inputs = input.split(' ');
+    const answers = answer.split(' ');
+
+    let positionCount = comparePosition(inputs, answers);
+    let includeCount = including(inputs, answers);
+
+    return `${positionCount}A${includeCount - positionCount}B`;
+}
+
+const comparePosition = (inputs, answers) => {
+    return inputs.filter((input, index) => answers[index] === input).length;
+}
+
+const including = (inputs, answers) => {
+    return inputs.filter(input => answers.includes(input)).length;
+}
+
+console.log(compareInputAnswer());
