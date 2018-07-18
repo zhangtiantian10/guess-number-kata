9278bec4e0489f1a470d0f536fa6455abedd4b89
Tian Zhang <tizhang@CNtizhang.local>
2018-07-18 16:04:41 +0800

    add input number to compare
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- input.js
+++ input.js
@@ -13,6 +13,14 @@ const inputNumber = () => {
 
 const checkInput = (input) => {
     const inputs = input.split(' ');
+    if (!checkInputIsNumberAndLength(inputs) || checkInputsIncludesSameItem(inputs)) {
+        return false;
+    }
+
+    return true;
+}
+
+const checkInputIsNumberAndLength = (inputs) => {
     const item = inputs.find((input) => {
         const charCode = input.charCodeAt(0);
         if ( charCode > 57 && charCode < 48) {
@@ -24,16 +32,24 @@ const checkInput = (input) => {
         if (number > 9 && number < 0) {
             return true;
         } else if (number)
-        
-        return false
+
+            return false
     })
+
     if (inputs.length > 4 || item) {
         return false;
     }
-
     return true;
 }
 
-inputNumber();
+const checkInputsIncludesSameItem = (inputs) => {
+    const item = inputs.find((input, index) => index !== inputs.lastIndexOf(input));
+
+    if (item) {
+        return true
+    }
+
+    return false;
+}
 
 module.exports = inputNumber;
