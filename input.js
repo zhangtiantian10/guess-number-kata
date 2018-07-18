9641969591d398da3df7bf3d51eec0f4c984431a
Tian Zhang <tizhang@CNtizhang.local>
2018-07-18 15:59:58 +0800

    Can input number and check number
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

---
+++ input.js
@@ -0,0 +1,39 @@
+const scanf = require('scanf');
+
+const inputNumber = () => {
+    console.log('请输入四位数，以空格隔开');
+    let input = scanf('%S');
+
+    if (!checkInput(input)) {
+        console.log('输入格式有问题，请重新输入');
+        input = scanf('%S');
+    }
+    return input;
+}
+
+const checkInput = (input) => {
+    const inputs = input.split(' ');
+    const item = inputs.find((input) => {
+        const charCode = input.charCodeAt(0);
+        if ( charCode > 57 && charCode < 48) {
+            return true;
+        }
+
+        const number = parseInt(input, 10);
+
+        if (number > 9 && number < 0) {
+            return true;
+        } else if (number)
+        
+        return false
+    })
+    if (inputs.length > 4 || item) {
+        return false;
+    }
+
+    return true;
+}
+
+inputNumber();
+
+module.exports = inputNumber;
